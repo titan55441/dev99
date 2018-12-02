189d694b1be7cdb627200c54f8995ff7591e6f89
titan <titan55441@gmail.com>
2018-12-02 12:56:07 +0800

    生态农业仿站完成！

---
+++ data/config.cache.inc.php
@@ -0,0 +1,186 @@
+<?php
+$cfg_disable_funs = 'phpinfo,eval,exec,passthru,shell_exec,system,proc_open,popen,curl_exec,curl_multi_exec,parse_ini_file,show_source,file_put_contents';
+$cfg_disable_tags = 'php';
+$cfg_basehost = 'http://dev99.com';
+$cfg_cmspath = '';
+$cfg_cookie_encode = 'lVEYEyQLu90qkr3YQCyWreIw1Nk5';
+$cfg_indexurl = '/';
+$cfg_backup_dir = 'backupdata';
+$cfg_indexname = '主页';
+$cfg_webname = '我的网站';
+$cfg_adminemail = 'admin@dedecms.com';
+$cfg_html_editor = 'ckeditor';
+$cfg_arcdir = '/a';
+$cfg_medias_dir = '/uploads';
+$cfg_ddimg_width = 240;
+$cfg_ddimg_height = 180;
+$cfg_domain_cookie = '';
+$cfg_imgtype = 'jpg|gif|png';
+$cfg_softtype = 'zip|gz|rar|iso|doc|xsl|ppt|wps';
+$cfg_mediatype = 'swf|mpg|mp3|rm|rmvb|wmv|wma|wav|mid|mov';
+$cfg_specnote = 6;
+$cfg_list_symbol = ' > ';
+$cfg_notallowstr = '非典|艾滋病|阳痿';
+$cfg_replacestr = '她妈|它妈|他妈|你妈|去死|贱人';
+$cfg_feedbackcheck = 'N';
+$cfg_keyword_replace = 'Y';
+$cfg_fck_xhtml = 'N';
+$cfg_df_style = 'shengtainongye';
+$cfg_multi_site = 'N';
+$cfg_dede_log = 'N';
+$cfg_powerby = 'Copyright &copy; 2002-2017 DEDECMS. 织梦科技 版权所有';
+$cfg_arcsptitle = 'N';
+$cfg_arcautosp = 'N';
+$cfg_arcautosp_size = 5;
+$cfg_auot_description = 240;
+$cfg_ftp_host = '';
+$cfg_ftp_port = 21;
+$cfg_ftp_user = '';
+$cfg_ftp_pwd = '';
+$cfg_ftp_root = '/';
+$cfg_ftp_mkdir = 'N';
+$cfg_feedback_ck = 'Y';
+$cfg_list_son = 'Y';
+$cfg_mb_open = 'N';
+$cfg_mb_album = 'Y';
+$cfg_mb_upload = 'Y';
+$cfg_mb_upload_size = 1024;
+$cfg_mb_sendall = 'Y';
+$cfg_mb_rmdown = 'Y';
+$cfg_cli_time = 8;
+$cfg_mb_addontype = 'swf|mpg|mp3|rm|rmvb|wmv|wma|wav|mid|mov|zip|rar|doc|xsl|ppt|wps';
+$cfg_mb_max = 500;
+$cfg_keyword_like = 'N';
+$cfg_index_max = 10000;
+$cfg_index_cache = 86400;
+$cfg_tplcache = 'Y';
+$cfg_tplcache_dir = '/data/tplcache';
+$cfg_makesign_cache = 'N';
+$cfg_rm_remote = 'Y';
+$cfg_arc_dellink = 'N';
+$cfg_arc_autopic = 'Y';
+$cfg_arc_autokeyword = 'Y';
+$cfg_title_maxlen = 60;
+$cfg_album_width = 800;
+$cfg_check_title = 'Y';
+$cfg_album_row = 3;
+$cfg_album_col = 4;
+$cfg_album_pagesize = 12;
+$cfg_album_style = 2;
+$cfg_album_ddwidth = 200;
+$cfg_mb_notallow = 'www,bbs,ftp,mail,user,users,admin,administrator';
+$cfg_mb_idmin = 3;
+$cfg_mb_pwdmin = 3;
+$cfg_mb_pwdtype = '32';
+$cfg_md_idurl = 'N';
+$cfg_mb_rank = 10;
+$cfg_feedback_time = 30;
+$cfg_feedback_numip = 30;
+$cfg_md_mailtest = 'N';
+$cfg_mb_spacesta = -10;
+$cfg_vdcode_member = 'Y';
+$cfg_mb_cktitle = 'Y';
+$cfg_mb_editday = 7;
+$cfg_caicai_sub = 2;
+$cfg_caicai_add = 2;
+$cfg_feedback_add = 5;
+$cfg_feedback_sub = 5;
+$cfg_search_max = 50000;
+$cfg_search_maxrc = 300;
+$cfg_search_time = 3;
+$cfg_baidunews_limit = '100';
+$cfg_updateperi = '15';
+$cfg_sendmail_bysmtp = 'Y';
+$cfg_smtp_server = 'smtp.qq.com';
+$cfg_smtp_port = '25';
+$cfg_smtp_usermail = 'desdev@vip.qq.com';
+$cfg_smtp_user = 'desdev';
+$cfg_smtp_password = '7260444huxiao';
+$cfg_online_type = 'nps';
+$cfg_upload_switch = 'Y';
+$cfg_allsearch_limit = '1';
+$cfg_rewrite = 'N';
+$cfg_delete = 'Y';
+$cfg_keywords = '';
+$cfg_description = '';
+$cfg_beian = '';
+$cfg_need_typeid2 = 'Y';
+$cfg_cache_type = 'id';
+$cfg_max_face = 50;
+$cfg_typedir_df = 'Y';
+$cfg_makeindex = 'N';
+$cfg_make_andcat = 'N';
+$cfg_make_prenext = 'Y';
+$cfg_feedback_forbid = 'N';
+$cfg_jump_once = 'Y';
+$cfg_task_pwd = '';
+$cfg_addon_domainbind = 'N';
+$cfg_addon_domain = '';
+$cfg_df_dutyadmin = 'admin';
+$cfg_mb_allowncarc = 'Y';
+$cfg_mb_allowreg = 'Y';
+$cfg_mb_adminlock = 'N';
+$cfg_sendarc_scores = 10;
+$cfg_sendfb_scores = 3;
+$cfg_mb_spaceallarc = 0;
+$cfg_face_adds = 10;
+$cfg_moreinfo_adds = 20;
+$cfg_money_scores = 50;
+$cfg_mb_wnameone = 'N';
+$cfg_arc_dirname = 'Y';
+$cfg_puccache_time = 36000;
+$cfg_arc_click = -1;
+$cfg_addon_savetype = 'ymd';
+$cfg_qk_uploadlit = 'Y';
+$cfg_login_adds = 2;
+$cfg_userad_adds = 10;
+$cfg_ddimg_full = 'N';
+$cfg_ddimg_bgcolor = 0;
+$cfg_replace_num = 2;
+$cfg_uplitpic_cut = 'Y';
+$cfg_album_mark = 'N';
+$cfg_mb_feedcheck = 'N';
+$cfg_mb_msgischeck = 'N';
+$cfg_mb_reginfo = 'Y';
+$cfg_remote_site = 'N';
+$cfg_title_site = 'N';
+$cfg_mysql_type = 'mysql';
+$cfg_sphinx_article = 'N';
+$cfg_sphinx_host = 'localhost';
+$cfg_sphinx_port = 9312;
+$cfg_memcache_enable = 'N';
+$cfg_memcache_mc_defa = 'memcache://127.0.0.1:11211/default127';
+$cfg_memcache_mc_oth = '';
+$cfg_cross_sectypeid = 'N';
+$cfg_digg_update = 0;
+$cfg_feedback_guest = 'N';
+$lxfs_dianhua = '<p>咨询热线</p>
+<p>123-000-0000</p>';
+$lxfs_qq = '<p>官方QQ</p>
+<p>123-000-0000</p>';
+$lxfs_sj = '<p>服务手机</p>
+<p>1230000000</p>';
+$shouye_jj = '富宝贝生态农庄一个被原生态360°包围的绿色空间，远离都市的嘈杂，农庄融入大自然怀抱，是你在喧闹的工作中放松休闲的一片静土。<br>农庄内设采摘区、垂钓区、田园果蔬区、活动区、摸鱼区、烧烤野炊等多项目娱乐项目，特别还有充满特色的KTV蒙古包。<br>富宝贝是一日游、亲子活动，南通周边游的首选之地。空气清新宜人，绝对是一个放松心情的好地方。';
+$jianjie_bigbang = '骨子里流动着电白人敢于改革、敢于拼搏、敢于开拓的精神，电白建设集团从未忘记迈出向前跨越的步履。海纳百川，谷涵万壑，百尺竿头，更进一步。如今的广东电白建设集团，以广东为依托，用实力、质量、品牌、诚信，将“建筑之乡”的光荣传统发扬光大，让遍布大江南北的琼楼玉宇、康庄大道、碧水绿林记住她湛蓝笃定的身姿。
+
+1979（诞生）
+
+1979年，电白建筑大业萌动发芽。而随着建筑行业的进一步发展，行业也慢慢呈现规范化、规模化的特点，在充满着各种可能性的时代，电白县林头建筑队应运而生。
+
+1986（雏形）
+
+1986年3月，电白在几年间如雨后春笋般纷纷成立建筑队伍，这也象征着电白建筑行业进入了全面生长期。行业需要敢为人先者的探索与追寻，不仅是探索企业组织形式规范化与标准化，更是探索建筑行业的梦想能有多大。电白县林头建筑工程';
+$jianjie_gsmc = '这里是公司名称';
+$jianjie_gsjja = '123海南和民生态农业有限公司以基地化、品质化、标准化、品牌化的发展战略为指引，
+                            目前，已发展成为一家集基地种植、加工包装、冷藏保鲜、批发配送等
+                            全产业链为一体的集团化公司。';
+$jianjie_gsjjb = '123和民生态农业致力于整合全球资源，引领先进的国际意识观，<br>
+                            我们用心把握海内外各产品的不同特性，针对性的进行种植、筛选和采购。<br>
+                            经过数十载的辛勤耕耘，公司业务发展遍布全球，成绩斐然。';
+$jianjie_gsjjc = '123和民生态农业更引进国际领先的商业推广理念，<br>
+                        开创鲜果行业多元化的商业格局，以求将中国的健康鲜果推向全球。';
+$jianjie_gsjjd = '<p>企业的成功体现于消费者的信赖，和民生态农业始终秉承包容汇聚的行业精神，</p>
+                    <p> 吸收行业日新月异的变化和需求，对品牌及产品持续投入真心、真意、真诚，确保品质如一。</p>
+                    <p>我们始终将客户的感受放在首位，保持高度的品牌渗透力和高效的客户服务力，</p>
+                    <p> 励志成为中国果业金字招牌，为千家万户带去安全健康、新鲜美味的优质果品。</p>';
+?>
